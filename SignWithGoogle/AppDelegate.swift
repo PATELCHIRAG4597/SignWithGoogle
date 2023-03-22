b72b14fc3a912c42cca90850f299881f5cb647c3
PATELCHIRAG4597 <124140359+PATELCHIRAG4597@users.noreply.github.com>
2023-03-22 10:08:21 +0530

    succesful

--- SignWithGoogle/AppDelegate.swift
+++ SignWithGoogle/AppDelegate.swift
@@ -3,25 +3,64 @@
 //  SignWithGoogle
 //
 //  Created by CodeInfoWay CodeInfoWay on 3/18/23.
-//
 
 import UIKit
-
+import FirebaseCore
+import FirebaseAuth
+import GoogleSignIn
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+//        GIDSignIn.sharedInstance.restorePreviousSignIn { user, error in
+//           if error != nil || user == nil {
+//            print ("Show the app's signed-out state.")
+//           } else {
+//            print ("Show the app's signed-in state.")
+//           }
+//         }
         // Override point for customization after application launch.
+        
+        FirebaseApp.configure()
+        //guard let clientID = FirebaseApp.app()?.options.clientID else { return }
+                
+        // Create Google Sign In configuration object.
+        let config = GIDConfiguration(clientID: "838256168929-qo9ga6i753bsc8anar5ooo2s2d30enro.apps.googleusercontent.com")
+                
+        GIDSignIn.sharedInstance.configuration = config
+        
         return true
     }
+    func application(
+      _ app: UIApplication,
+      open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]
+    ) -> Bool {
+      var handled: Bool
 
-    // MARK: UISceneSession Lifecycle
+      handled = GIDSignIn.sharedInstance.handle(url)
+      if handled {
+        return true
+      }
 
+      // Handle other custom URL types.
+
+      // If not handled by this app, return false.
+      return false
+    }
+//
+//     func application(_ app: UIApplication,
+//                          open url: URL,
+//                          options: [UIApplication.OpenURLOptionsKey: Any] = [:]) -> Bool {
+//         let handle =   GIDSignIn.sharedInstance.handle(url)
+//        return handle
+//         }
+    
+    
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        // Called when a new scene session is being created.
-        // Use this method to select a configuration to create the new scene with.
+        
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
 
--- SignWithGoogle/ViewController.swift
+++ SignWithGoogle/ViewController.swift
@@ -3,17 +3,24 @@
 //  SignWithGoogle
 //
 //  Created by CodeInfoWay CodeInfoWay on 3/18/23.
-//
-
 import UIKit
-
+import GoogleSignIn
 class ViewController: UIViewController {
-
+    
+    let signInconfig = GIDConfiguration.init(clientID:"838256168929-qo9ga6i753bsc8anar5ooo2s2d30enro.apps.googleusercontent.com")
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
+    @IBAction func GoogleBtn(_ sender: Any) {
+        GIDSignIn.sharedInstance.signIn(withPresenting: self) { signInResult, error in
+            guard error == nil else { return }
+            guard let signInResult = signInResult else { return }
 
+               let user = signInResult.user
+               let emailAddress = user.profile?.email
+
+       }
+    
+    }
 }
-
